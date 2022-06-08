bb4edf6e8b4fedb4f23a3e064ee2aabd0cf72c8c
CristianMogensen <cristian.g.mog@gmail.com>
2022-06-08 12:29:02 -0300

    Corrección de formato de pycodestyle.

--- src/practica/ejercicio5.py
+++ src/practica/ejercicio5.py
@@ -42,32 +42,32 @@ def hay_balanceo_corchetes(texto: str) -> bool:
     PRECONDICIONES: Recibe un texto (string).
     POSCONDICIONES: Devuelve un bool (True o False).
     """
-    
+
     # Verifico que el texto (variable 'texto') de entrada sea de tipo
     # string (str).
     assert (isinstance(texto, str)), "El 'texto' ingresado no es un string."
-    
+
     # Declaro y defino una variable en la que se contará en qué "nivel"
     # se encuentra de los corchetes. Por ejemplo [nivel1[nivel2]].
     nivel: int = 0
-    
+
     # Recorro todo el texto ingresado en el argumento de la función.
     for elemento in texto:
-        
+
         # Si el elemento es el corchete que "abre", entonces el nivel
         # aumentará en 1.
         if (elemento == '['):
-            
+
             nivel += 1
-            
+
         # Sino, si es el corchete que "cierra", entonces el nivel
         # decrecerá en 1, sólo si ya hubo un símbolo de 'inicio'.
         elif (elemento == ']'):
-            
+
             if (nivel > 0):
-                
+
                 nivel -= 1
-            
+
     # Finalmente, se verifica que el nivel sea el mismo con el que
     # se comenzó (cero). Si se cumple, entonces retornará True, y
     # sino False.
@@ -88,54 +88,60 @@ def hay_balanceo_especifico(texto: str,
                     'cierre').
     POSCONDICIONES: Devuelve un bool (True o False).
     """
-    
+
     # Verifico que el texto (variable 'texto') de entrada sea de tipo
     # string (str).
     assert (isinstance(texto, str)), "El 'texto' ingresado no es un string."
-    
+
     # Verifico que el símbolo de "apertura" (variable 'inicio'),
     # ingresado en la entrada o argumento de la función, sea de tipo
     # string (str).
     assert (isinstance(inicio, str)), "El 'inicio' ingresado no es un string."
-    
+
     # Verifico que el símbolo de "cierre" (variable 'cierre'),
     # ingresado en la entrada o argumento de la función, sea de tipo
     # string (str).
     assert (isinstance(cierre, str)), "El 'cierre' ingresado no es un string."
-    
+
     # Verifico que los símbolos ingresados para el 'inicio' y 'cierre'
     # sean distintos. De esta manera, se puede distinguir si se "abre"
     # o se "cierra".
-    assert (inicio != cierre), "Los símbolos de apertura y cierre no pueden ser iguales."
-    
+    # Uso variable para seguir el formato de pycodestyle.
+    error1: str = "Los símbolos de apertura y cierre no pueden ser iguales."
+    assert (inicio != cierre), error1
+
     # Verifico que la longitud del string 'inicio' sea 1.
-    assert (len(inicio) == 1), "El símbolo de 'inicio' posee más de un caracter."
-    
+    # Uso variable para seguir el formato de pycodestyle.
+    error2: str = "El símbolo de 'inicio' posee más de un caracter."
+    assert (len(inicio) == 1), error2
+
     # Verifico que la longitud del string 'cierre' sea 1.
-    assert (len(cierre) == 1), "El símbolo de 'cierre' posee más de un caracter."
-    
+    # Uso variable para seguir el formato de pycodestyle.
+    error3: str = "El símbolo de 'cierre' posee más de un caracter."
+    assert (len(cierre) == 1), error3
+
     # Declaro y defino una variable en la que se contará en qué "nivel"
     # se encuentra de los corchetes, paréntesis, o símbolo
     # especificado. Por ejemplo (nivel1(nivel2)).
     nivel: int = 0
-    
+
     # Recorro todo el texto ingresado en el argumento de la función.
     for elemento in texto:
-        
+
         # Si el elemento es el corchete que "abre", entonces el nivel
         # aumentará en 1.
         if (elemento == inicio):
-            
+
             nivel += 1
-            
+
         # Sino, si es el corchete que "cierra", entonces el nivel
         # decrecerá en 1, sólo si ya hubo un símbolo de 'inicio'.
         elif (elemento == cierre):
-            
+
             if (nivel > 0):
-                
+
                 nivel -= 1
-            
+
     # Finalmente, se verifica que el nivel sea el mismo con el que
     # se comenzó (cero). Si se cumple, entonces retornará True, y
     # sino False.
@@ -171,15 +177,15 @@ def principal():
     Esta función es la que se encarga de la parte 'interactiva' del ejercicio
     (La entrada, la llamada al algoritmo y la salida)
     """
-    
+
     # Imprimo título:
     print("Ejercicio 5:\tCorchetes balanceados")
-    
+
     # Input:
     print("Ingrese texto para verificar que los corchetes estén balanceados:")
-    
+
     entrada: str = input()
-    
+
     # Salida:
     print(f"¿Está balanceado? {hay_balanceo_corchetes(entrada)}")
 

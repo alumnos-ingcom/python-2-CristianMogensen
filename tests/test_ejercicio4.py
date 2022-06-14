61ddf39b07e68fae113ad0581d68b80dbd9686e9
CristianMogensen <cristian.g.mog@gmail.com>
2022-06-14 17:27:20 -0300

    Test de Ejercicio 4 terminado.

--- tests/test_ejercicio4.py
+++ tests/test_ejercicio4.py
@@ -3,17 +3,97 @@
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
-from practica.plantilla import principal
+from practica.ejercicio4 import fibonacci
 
 """
-Describir aquí que es lo que se esta probando.
-Tengan en cuenta que el archivo tiene que llamarse igual
-que el archivo a probar agregando antes `test_`
+Tests del Ejercicio 4: Fibonacci.
+
+Se buscan testear 5 casos de prueba para la función fibonacci:
+
+fibonacci(termino: int):
+1- termino = 1. Resultado esperado: 1
+2- termino = 2. Resultado esperado: 1
+3- termino = 3. Resultado esperado: 2
+4- termino = 4. Resultado esperado: 3
+5- termino = 10. Resultado esperado: 55
 """
 
 
-def test_nombrefuncion():
+def test_fibonacci_primer_termino():
     """
-    Una breve descripción del caso de prueba aplicado a la función
+    Test función fibonacci(termino)
+    Caso de prueba 1:
+    - Entrada: termino = 1
+    - Resultado esperado: 1
     """
-    pass
+
+    termino: int = 1
+
+    resultado: int = fibonacci(termino)
+
+    assert (isinstance(resultado, int)), "El resultado debe ser de tipo int."
+    assert (resultado == 1), "El resultado es distinto del esperado."
+
+
+def test_fibonacci_segundo_termino():
+    """
+    Test función fibonacci(termino)
+    Caso de prueba 2:
+    - Entrada: termino = 2
+    - Resultado esperado: 1
+    """
+
+    termino: int = 2
+
+    resultado: int = fibonacci(termino)
+
+    assert (isinstance(resultado, int)), "El resultado debe ser de tipo int."
+    assert (resultado == 1), "El resultado es distinto del esperado."
+
+
+def test_fibonacci_tercer_termino():
+    """
+    Test función fibonacci(termino)
+    Caso de prueba 3:
+    - Entrada: termino = 3
+    - Resultado esperado: 2
+    """
+
+    termino: int = 3
+
+    resultado: int = fibonacci(termino)
+
+    assert (isinstance(resultado, int)), "El resultado debe ser de tipo int."
+    assert (resultado == 2), "El resultado es distinto del esperado."
+
+
+def test_fibonacci_cuarto_termino():
+    """
+    Test función fibonacci(termino)
+    Caso de prueba 4:
+    - Entrada: termino = 4
+    - Resultado esperado: 3
+    """
+
+    termino: int = 4
+
+    resultado: int = fibonacci(termino)
+
+    assert (isinstance(resultado, int)), "El resultado debe ser de tipo int."
+    assert (resultado == 3), "El resultado es distinto del esperado."
+
+
+def test_fibonacci_decimo_termino():
+    """
+    Test función fibonacci(termino)
+    Caso de prueba 5:
+    - Entrada: termino = 10
+    - Resultado esperado: 55
+    """
+
+    termino: int = 10
+
+    resultado: int = fibonacci(termino)
+
+    assert (isinstance(resultado, int)), "El resultado debe ser de tipo int."
+    assert (resultado == 55), "El resultado es distinto del esperado."
